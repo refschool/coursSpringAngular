68290e082df8eb38bf3f5a34920249dd9949534c
Thierry1shot <tquan2206@gmail.com>
2026-06-16 11:12:21 +0200

    ajouter utilisateur et tester

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.entities.utilisateur;
+import com.example.demo.repository.utilisateurRepository;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private utilisateurRepository userRepository;
+
 	@Test
-	void contextLoads() {
+	public void testCreateUser() {
+		utilisateur user = new utilisateur("Huynh", "Yvon", "test@test.com", "123", "test", LocalDateTime.now());
+		userRepository.save(user);
 	}
 
 }
