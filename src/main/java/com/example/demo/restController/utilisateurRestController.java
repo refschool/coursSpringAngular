0f11f7071363aac14dfd9cfbba33a27b95cc9158
Thierry1shot <tquan2206@gmail.com>
2026-06-17 16:21:20 +0200

    créer WebServices et lister tous les utilisateurs

---
+++ src/main/java/com/example/demo/restController/utilisateurRestController.java
@@ -0,0 +1,26 @@
+package com.example.demo.restController;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.utilisateur;
+import com.example.demo.service.utilisateurService;
+
+@RestController
+@RequestMapping("/api")
+public class utilisateurRestController {
+
+    private final utilisateurService utilisateurService;
+
+    public utilisateurRestController(utilisateurService utilisateurService) {
+        this.utilisateurService = utilisateurService;
+    }
+
+    @GetMapping
+    public List<utilisateur> getAllUtilisateurs() {
+        return utilisateurService.getAllUtilisateurs();
+    }
+}
