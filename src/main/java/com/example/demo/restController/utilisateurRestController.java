2240556bfabe01314419753c57f271d0244aed44
Thierry1shot <tquan2206@gmail.com>
2026-06-17 20:37:51 +0200

    Rest delete user

--- src/main/java/com/example/demo/restController/utilisateurRestController.java
+++ src/main/java/com/example/demo/restController/utilisateurRestController.java
@@ -2,6 +2,7 @@ package com.example.demo.restController;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,4 +43,10 @@ public class utilisateurRestController {
     public utilisateur updateUtilisateur(@RequestBody utilisateur u) {
         return utilisateurService.updateUtilisateur(u);
     }
+
+    @DeleteMapping("/{id}")
+    public void deleteUtilisateur(@PathVariable("id") Long id) {
+        utilisateurService.deleteutilisateurById(id);
+    }
+
 }
