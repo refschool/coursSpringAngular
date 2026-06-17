b414e3271b3dab9401d77351424e021bec18f3d0
Thierry1shot <tquan2206@gmail.com>
2026-06-17 19:56:27 +0200

    Rest get user by id

--- src/main/java/com/example/demo/restController/utilisateurRestController.java
+++ src/main/java/com/example/demo/restController/utilisateurRestController.java
@@ -3,6 +3,7 @@ package com.example.demo.restController;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +24,10 @@ public class utilisateurRestController {
     public List<utilisateur> getAllUtilisateurs() {
         return utilisateurService.getAllUtilisateurs();
     }
+
+    @GetMapping("/{id}")
+    public utilisateur getUtilisateurById(@PathVariable("id") Long id) {
+        return utilisateurService.getutilisateur(id);
+    }
+
 }
