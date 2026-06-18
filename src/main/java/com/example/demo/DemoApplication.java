c74033e550498ab9e2330803fe83462d69390ddb
Thierry1shot <tquan2206@gmail.com>
2026-06-18 14:42:15 +0200

    SrpingDataRest avoir les id utilisateurs

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+import com.example.demo.entities.utilisateur;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+	private final RepositoryRestConfiguration repositoryRestConfiguration;
+
+	DemoApplication(RepositoryRestConfiguration repositoryRestConfiguration) {
+		this.repositoryRestConfiguration = repositoryRestConfiguration;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		repositoryRestConfiguration.exposeIdsFor(utilisateur.class);
+	}
 }
