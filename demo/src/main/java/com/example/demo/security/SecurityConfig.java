68cae2a1deec85190d93aa3996a1aa2cfe5402d0
Thierry1shot <tquan2206@gmail.com>
2026-07-02 11:14:06 +0200

    Copier-Coller JWTAuthorizationFilter, SecParams et SecurityConfig(puis le modifier un peu)

---
+++ demo/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http,
+            AuthenticationManager authMgr) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
