6db2c73721274f836ccc54e10b5103bfacd56eb2
Thierry1shot <tquan2206@gmail.com>
2026-07-08 15:04:29 +0200

    Créer la classe Modèle Prospect

---
+++ Angular/MesProspects/src/app/model/prospect.model.ts
@@ -0,0 +1,10 @@
+export class Prospect {
+    id_prospect?: number;
+    nom?: string;
+    prenom?: string;
+    email?: string;
+    telephone?: string;
+    entreprise?: string;
+    source?: string;
+    date_creation?: Date;
+}
--- Angular/MesProspects/src/app/prospects/prospects.ts
+++ Angular/MesProspects/src/app/prospects/prospects.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Prospect } from '../model/prospect.model';
+
 
 @Component({
   selector: 'app-prospects',
@@ -7,8 +9,28 @@ import { Component } from '@angular/core';
   templateUrl: './prospects.html'
 })
 export class Prospects {
-  prospects: string[];
+  prospects: Prospect[];
   constructor() {
-    this.prospects = ["Yvon", "Thierry", "Emma"];
+    this.prospects = [
+      {
+        id_prospect: 1,
+        nom: "Durand",
+        prenom: "Alice",
+        email: "alice.durand@example.com",
+        telephone: "06 45 23 89 12",
+        entreprise: "TechNova",
+        source: "LinkedIn",
+        date_creation: new Date("2025-11-12")
+      },
+      {
+        id_prospect: 2,
+        nom: "Martin",
+        prenom: "Lucas",
+        email: "lucas.martin@example.com",
+        telephone: "07 81 22 45 90",
+        entreprise: "GreenSoft",
+        source: "Salon professionnel",
+        date_creation: new Date("2025-10-03")
+      }]
   }
 }
