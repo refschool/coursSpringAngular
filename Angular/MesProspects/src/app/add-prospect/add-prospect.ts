9bcec937747f70aadf998211d3565375b0c96ccf
Thierry1shot <tquan2206@gmail.com>
2026-07-08 16:48:51 +0200

    Créer formulaire pour ajouter des prospects

--- Angular/MesProspects/src/app/add-prospect/add-prospect.ts
+++ Angular/MesProspects/src/app/add-prospect/add-prospect.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { Prospect } from '../model/prospect.model';
 @Component({
   selector: 'app-add-prospect',
-  imports: [],
+  imports: [FormsModule
+  ],
   templateUrl: './add-prospect.html',
   styleUrl: './add-prospect.css',
 })
-export class AddProspect {}
+export class AddProspect {
+  newProspect = new Prospect();
+  addProspect() {
+    console.log(this.newProspect);
+  }
+
+}
