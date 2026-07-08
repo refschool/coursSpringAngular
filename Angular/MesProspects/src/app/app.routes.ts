b306a647cd6e07990118e99816602b3b582a3f7a
Thierry1shot <tquan2206@gmail.com>
2026-07-08 11:35:48 +0200

    Créer un composant prospects

--- Angular/MesProspects/src/app/app.routes.ts
+++ Angular/MesProspects/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Prospects } from './prospects/prospects';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: "prospects", component: Prospects }
+];
---
+++ Angular/MesProspects/src/app/prospects/prospects.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prospects',
+  imports: [],
+  templateUrl: './prospects.html'
+})
+export class Prospects { }
